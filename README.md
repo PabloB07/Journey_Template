fcd625a8def368f57d204b3ee744aec25d09636e
BuildTools <unconfigured@null.spigotmc.org>
2021-08-08 19:16:20 -0400

    Last changes template.rb

---
+++ README.md
@@ -0,0 +1,45 @@
+# Journey - Tailwind CSS
+A rapid Rails 6 application template for personal use. This particular template utilizes [Tailwind CSS](https://tailwindcss.com/), a utility-first CSS framework for rapid UI development.
+
+Tailwind depends on Webpack so this also comes bundled with [webpacker](https://github.com/rails/webpacker) support.
+
+### Included gems
+
+- [devise](https://github.com/plataformatec/devise)
+- [friendly_id](https://github.com/norman/friendly_id)
+- [sidekiq](https://github.com/mperham/sidekiq)
+- [name_of_person](https://github.com/basecamp/name_of_person)
+- [omniauth-twitter](https://github.com/arunagw/omniauth-twitter)
+
+### Tailwind CSS by default
+With Rails 6 we have webpacker by default now. Using PostCSS we can install Tailwind as a base CSS framework to harness. I prefer Tailwind due to it's un-opinionated approach.
+
+## How it works
+
+When creating a new rails app simply pass the template file through.
+
+### Creating a new app
+
+```ruby
+rails new sample_app -d <postgresql, mysql, sqlite3> -m template.rb
+```
+### Remember to add Twitter secret & api Key
+```ruby
+EDITOR=nano rails credentials:edit
+```
+### and finally
+```ruby
+foreman start
+```
+### Once installed what do I get?
+
+- Webpack support + Tailwind CSS configured in the `app/javascript` directory.
+- Devise with a new `name` field already migrated in. The name field maps to the `first_name` and `last_name` fields in the database thanks to the `name_of_person` gem. 
+- Enhanced views using Tailwind CSS.
+- Support for Friendly IDs thanks to the handy [friendly_id](https://github.com/norman/friendly_id) gem. Note that you'll still need to do some work inside your models for this to work. This template installs the gem and runs the associated generator.
+- Optional Foreman support thanks to a `Procfile`. Once you scaffold the template, run `foreman start` to initalize and head to `locahost:5000` to get `rails server`, `sidekiq` and `bin/webpack-dev-server` running all in one terminal instance. Note: Webpack will still compile down with just `rails server` if you don't want to use Foreman. Foreman needs to be installed as a global gem on your system for this to work. i.e. `gem install foreman`
+- A custom scaffold view template when generating theme resources (Work in progress).
+- Git initialization out of the box
+- PurgeCSS configuration to help with CSS file sizes 
+- Custom defaults for button and form elements
+- Omniauth with Twitter all views, model, controllers and shared are improved
---
+++ template.rb
@@ -0,0 +1,131 @@
+=begin
+Template Name: Kickoff - Tailwind CSS, Template Renamed as: Journey - Tailwind CSS
+Author: Andy Leverenz <-- All credit to him.
+Edited by: Pablo Blanco
+Author URI: https://web-crunch.com, https://github.com/PabloB07
+Instructions: $ rails new myapp -d <postgresql, mysql, sqlite3> -m template.rb
+and finally: foreman start
+=end
+class Template < ::Rails::Generators::Base
+
+  source_root File.expand_path('', __FILE__)
+  desc 'Journey template, install Template and generate the following source files for your app!'
+
+def add_gems
+  gem 'devise', '~> 4.7', '>= 4.7.3'
+  gem 'friendly_id', '~> 5.4', '>= 5.4.1'
+  gem 'sidekiq', '~> 6.1', '>= 6.1.2'
+  gem 'name_of_person', '~> 1.1', '>= 1.1.1'
+  gem 'omniauth'
+  gem 'omniauth-twitter'
+  gem 'font-awesome-rails'
+end
+
+def add_users
+  # Install Devise
+  generate "devise:install"
+
+  # Configure Devise
+  environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }",
+              env: 'development'
+  route "root to: 'home#index'"
+
+  # Create Devise User
+  generate :devise, "User", "first_name", "last_name", "admin:boolean"
+
+  insert_into_file "config/routes.rb",
+    ", controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }\n\n",
+    after: "devise_for :users"
+
+    omniauth = <<-RUBY
+      config.omniauth :twitter, Rails.application.credentials.fetch(:twitter_api_key), Rails.application.credentials.fetch(:twitter_api_secret)
+      insert_into_file "devise.rb","#{omniauth}\n\n", after: "Devise.setup do |config|\n"
+  RUBY
+
+  # Set admin boolean to false by default
+  in_root do
+    migration = Dir.glob("db/migrate/*").max_by{ |f| File.mtime(f) }
+    gsub_file migration, /:admin/, ":admin, default: false"
+  end
+end
+
+def copy_templates
+  directory "app", force: true
+end
+
+def add_tailwind
+  # Until PostCSS 8 ships with Webpacker/Rails we need to run this compatability version
+  # See: https://tailwindcss.com/docs/installation#post-css-7-compatibility-build
+  run "yarn add tailwindcss@npm:@tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9"
+  run "cp -r node_modules/tailwindcss/dist/tailwind app/javascript/stylesheets"
+  append_to_file("app/javascript/packs/application.js", 'import "stylesheets/application"')
+  inject_into_file("./postcss.config.js", "\n    require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),", after: "plugins: [")
+  run "mkdir -p app/javascript/stylesheets/components"
+end
+
+# Remove Application CSS
+def remove_app_css
+  remove_file "app/assets/stylesheets/application.css"
+end
+
+def add_sidekiq
+  environment "config.active_job.queue_adapter = :sidekiq"
+
+  insert_into_file "config/routes.rb",
+    "require 'sidekiq/web'\n\n",
+    before: "Rails.application.routes.draw do"
+
+  content = <<-RUBY
+    authenticate :user, lambda { |u| u.admin? } do
+      mount Sidekiq::Web => '/sidekiq'
+    end
+  RUBY
+  insert_into_file "config/routes.rb", "#{content}\n\n", after: "Rails.application.routes.draw do\n"
+end
+
+def add_foreman
+  run "touch Procfile" if OS.osx? || OS.unix?
+  run "type nul > Procfile" if OS.windows?
+  copy_file "Procfile"
+end
+
+def add_friendly_id
+  generate "friendly_id"
+end
+
+# Main setup
+
+add_gems
+
+after_bundle do
+  add_users
+  remove_app_css
+  add_sidekiq
+  add_foreman
+  copy_templates
+  add_tailwind
+  add_friendly_id
+
+  # Migrate & create a migration named add_omniauth_to_users
+  rails_command "db:create"
+  rails_command "db:migrate"
+  rails_command "generate migration AddOmniauthToUsers provider:string uid:string"
+  rails_command "db:migrate"
+
+  git :init
+  git add: "."
+  git commit: %Q{ -m "Initial commit :fire: :package:" }
+
+  say
+  say "🥳 Project successfully created, with this Template! 💎", :green
+  say
+  say "Switch to your app by running:"
+  say "$ cd #{app_name}", :yellow
+  say
+  say "2 Ways to run:", :yellow
+  say "$ rails server", :green
+  say ""
+  say "(foreman run web, sidekiq, fiendly_id, Webpack & services)", :yellow
+  say "$ foreman start", :green
+  end
+end
