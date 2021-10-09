758737da56c36b7f1f9c18425abac288374a8b94
PabloB07 <pabloblanco0798@gmail.com>
2021-10-09 12:45:04 -0300

    Oops!

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,6 @@ class User < ApplicationRecord
     # If you are using confirmable and the provider(s) you use validate emails,
     # uncomment the line below to skip the confirmation emails.
     # user.skip_confirmation!
-    end
   end
 
   def email_required?
